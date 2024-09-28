299e35f41056ebbe67a2e1bf96316048c35303cd
Matt Baer <matt@write.as>
2024-09-28 18:05:25 -0400

    Move import action to its own command

--- cmd/wpimport/main.go
+++ cmd/wpimport/main.go
@@ -12,19 +12,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/urfave/cli/v2"
-	wpimport "github.com/writeas/wp-import"
 	"github.com/writeas/wp-import/core"
-	"golang.org/x/term"
 )
 
-func main() {
-	var fname string
+var fname string
 
+func main() {
 	app := &cli.App{
 		Name:    "WriteFreely WordPress importer",
 		Usage:   "Import a WordPress blog into Write.as/WriteFreely by running this importer on an exported WXR file.",
@@ -38,75 +35,21 @@ func main() {
 				Destination: &fname,
 			},
 		}...),
-		Action: func(c *cli.Context) error {
-			fmt.Println("Hello", core.Username)
-
-			fmt.Println("Please enter your password:")
-			var enteredPassword string
-			for {
-				password, err := term.ReadPassword(0)
-				if err != nil {
-					panic(err)
-				}
-				if len(password) != 0 {
-					fmt.Println("Press Return to log in and start the migration.")
-					enteredPassword = string(password)
-				} else {
-					break
-				}
-			}
-			err := core.SignIn(core.Username, enteredPassword, core.InstanceURL)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			//fmt.Println("Importing content from content ->", srcPath)
-			fmt.Println("Importing content into blog alias ->", core.DstBlog)
-
-			log.Printf("Reading %s...\n", fname)
-			raw, err := ioutil.ReadFile(fname)
-			if err != nil {
-				core.SignOut()
-				log.Fatal(err)
-			}
-
-			log.Println("Parsing...")
-			err = wpimport.ImportWordPress(core.DstBlog, raw)
-			if err != nil {
-				core.SignOut()
-				log.Fatal(err)
-			}
-			/*
-				posts, err := ParseContentDirectory(srcPath, uploadImages)
-				if err != nil {
-					SignOut()
-					log.Fatal(err)
-				}
-			*/
-
-			/*
-				fmt.Println("")
-				fmt.Println("Publishing posts to", core.DstBlog)
-				for _, post := range posts {
-					err := PublishPost(post, core.DstBlog)
-					if err != nil {
-						core.SignOut()
-						log.Fatal(err)
-					}
-				}
-				fmt.Println("Posts published.")
-			*/
-
-			core.SignOut()
-
-			/*
-				err = WriteResponsesToDisk()
-				if err != nil {
-					log.Fatal(err)
-				}
-			*/
-
-			return nil
+		Commands: []*cli.Command{
+			{
+				Name:   "import",
+				Action: CmdImport,
+				Usage:  "Import WordPress export file into WriteFreely",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "filename",
+						Aliases:     []string{"f"},
+						Usage:       "",
+						Required:    true,
+						Destination: &fname,
+					},
+				},
+			},
 		},
 	}
 
